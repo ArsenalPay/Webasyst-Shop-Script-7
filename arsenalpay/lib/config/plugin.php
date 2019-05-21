aeaada6bd08ba84f21875c90b3ac33a1e9995494
Eugene Radovets <eugene.radovets@arsenalmedia.ru>
2019-05-21 13:25:42 +1000

    3922 Add fiscal structure OFD

--- arsenalpay/lib/config/plugin.php
+++ arsenalpay/lib/config/plugin.php
@@ -1,7 +1,7 @@
 <?php
 return array(
   'name' => 'ArsenalPay',
-  'description' => /*_wp*/('Платежная система <a href="https://www.arsenalpay.ru">ArsenalPay</a>'),
+  'description' => /*_wp*/('Платежная система <a href="https://www.arsenalpay.ru">ArsenalPay</a>. Оплата с карт Visa/MasterCard/МИР'),
   // other optional parameters 
   'icon' => 'img/arsenalpay16.png', // 16x16px large icon (to be displayed in the Installer) 
   // default payment gateway logo
@@ -9,7 +9,7 @@ return array(
   // plugin vendor ID (for 3rd parties vendors it's a number)
   'vendor' => 1032647,
   // plugin version
-  'version' => '1.0.2',
+  'version' => '1.0.3',
   'locale' => array('ru_RU', ),
   'type' => waPayment::TYPE_ONLINE,
 );
